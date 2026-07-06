f45c3769cf38b7d727072c9c50fc8df4484a7075
Juan Rodriguez <juanprodrigues.33@gmail.com>
2026-07-05 21:41:43 -0300

    feat: class organization

--- ms-auth/src/main/java/com/msauth/exception/GlobalExceptionHandler.java
+++ ms-auth/src/main/java/com/msauth/exception/GlobalExceptionHandler.java
@@ -1,16 +1,17 @@
 package com.msauth.exception;
 
-import jakarta.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.empresa.commons.response.ErrorResponse;
+import com.commons.response.ErrorResponse;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
